a132a8b317eabbfc38605ef0172ee1c30d7a299c
Theran Slinkard <theranslinkard@gmail.com>
2024-01-29 08:08:43 -0600

    gettin there

--- lua/alpaca.lua
+++ lua/alpaca.lua
@@ -1,12 +1,18 @@
 local vim = vim
-  if not handle
 local uv = vim.uv or vim.loop
 
 AlpacaPath = vim.fn.stdpath("data") .. "/site/pack/alpaca"
 
+local function to_array(inp)
+  if type(inp) == "string" then
+    return { inp }
+  else
+    return inp
+  end
+end
+
 ---@class Git
 Git = {}
-
 ---@param pipe userdata
 ---@param callback fun(err: string?, data: string?): nil
 function Git:read_pipe(pipe, callback)
@@ -21,10 +27,11 @@ function Git:read_pipe(pipe, callback)
     end
   end)
 end
+
 ---@param args string[]
 ---@param cwd string?
 ---@param callback fun(err: string?, ok: boolean, stdout: string?, stderr: string?): nil
-function Git:spawn_cap(args, cwd, callback)
+function Git:spawn(args, cwd, callback)
   local stdout = uv.new_pipe(false)
   local stderr = uv.new_pipe(false)
   local handle = uv.spawn("git", {
@@ -47,65 +54,124 @@ function Git:spawn_cap(args, cwd, callback)
   end
 end
 
-function Git:spawn(args, cwd, callback)
-  local handle = uv.spawn("git", {
-    args = args, cwd = cwd, stdio = { nil, 1, 2 }
-  }, function(code)
-    callback(nil, code == 0)
-  end)
-  if not handle then
-    callback(string.format("Failed to spawn git with args ( %s ) at ( %s )", vim.iter(args):join(" "), cwd), false)
-  end
----@param plugin Plugin
----@param callback fun(ok: boolean): nil
-function Git:clone(plugin, callback)
-  self:spawn({ "--depth=1", plugin.url, plugin.path }, nil, function(ok)
-    callback(ok)
-  end)
-end
-
 function Git:fetch(plugin, callback)
 end
 
 function Git:checkout(plugin, callback)
 end
 
+---@class PluginSpec
+---@field [1] string
+---@field alias string?
+---@field config function?
+---@field branch string?
+---@field tag string?
+---@field event (string | string[])?
+---@field cmd (string | string[])?
+---@field ft (string | string[])?
+
+---@class Plugin
+---@field name string
+---@field url string
+---@field path string
+---@field config function?
+---@field branch string?
+---@field tag string?
+---@field event string[]?
+---@field cmd string[]?
+---@field ft  string[]?
+
 Plugin = {}
-Alpaca = {}
+---@param spec string | PluginSpec
+---@return Plugin
+function Plugin:new(spec)
+  spec = to_array(spec)
 
----@package
----@param cmd string
----@param args string[]
----@param cwd string?
----@param callback function
-local function spawn(cmd, args, cwd, callback)
-  local stderr = uv.new_pipe(false)
-  local spawn_args = { args = args, cwd = cwd, stdio = { nil, nil, stderr } }
+  local plugin = setmetatable({}, self)
+  self.__index = self
 
-  local handle, _ = uv.spawn(cmd, spawn_args, function(code)
-    local buffer = ""
-    if code == 0 then
-      callback(nil)
-    else
-      stderr:read_start(function(err, data)
-        assert(not err, err)
-        if data then
-          buffer = buffer .. data
-        else
-          stderr:read_stop()
-          stderr:close()
-          buffer = string.gsub(buffer, "\n", "")
-          callback(buffer)
-        end
-      end)
+  plugin.name = spec.alias and spec.alias or vim.split(spec[1], "/")[2]
+  plugin.url = "https://github.com/" .. spec[1]
+  plugin.path = AlpacaPath .. ((spec.event or spec.cmd or spec.ft) and "/opt/" or "/start/") .. plugin.name
+
+  plugin.config = spec.config
+  plugin.event = spec.event and to_array(spec.event)
+  plugin.cmd = spec.cmd and to_array(spec.cmd)
+  plugin.ft = spec.ft and to_array(spec.ft)
+
+  plugin.branch = spec.branch
+  plugin.tag = spec.tag
+
+  return plugin
+end
+
+---@return boolean
+function Plugin:installed()
+  return uv.fs_stat(self.path) and true or false
+end
+
+---@return boolean
+function Plugin:updated()
+  Git:fetch(self, function(ok)
+    if ok then
+
+    end
+  end)
+  return false
+end
+
+Alpaca = {
+  plugins = {},
+  to_install = {},
+  to_update = {},
+  to_remove = {}
+}
+
+function Alpaca:install()
+end
+
+function Alpaca:update()
+end
+
+function Alpaca:remove()
+end
+
+function Alpaca:load()
+end
+
+---@param specs (string | PluginSpec)[]
+function Alpaca:setup(specs)
+  vim.iter(specs):map(function(spec)
+    return Plugin:new(spec)
+  end):each(function(plugin)
+    if not plugin:installed() then
+      table.insert(self.to_install, plugin)
+    elseif not plugin:updated() then
+      table.insert(self.to_update, plugin)
+    end
+    table.insert(self.plugins, plugin)
+  end)
+  vim.iter(vim.fs.dir(AlpacaPath, { depth = 2 })):map(function(name, type)
+    if name ~= "opt" and name ~= "start" and type == "directory" then
+      local split = vim.split(name, "/")
+      return { opt = split[1] == "opt" and true or false, name = split[2] }
+    end
+  end):each(function(installed)
+    local loaded = vim.iter(self.plugins):map(function(plugin)
+      return { name = plugin.name, opt = (plugin.event or plugin.cmd or plugin.ft) and true or false }
+    end):find(function(plugin)
+      return vim.deep_equal(plugin, installed)
+    end)
+    if not loaded then
+      table.insert(self.to_remove, installed)
     end
   end)
 
-  if not handle then
-    vim.notify("Failed to spawn git")
-  end
+  self:install()
+  self:update()
+  self:remove()
+  self:load()
 end
-
 
 Plugins = {
   plugins = {},
@@ -249,167 +315,3 @@ Installed = {
     self:clean_any()
   end,
 }
-
----@package
----@param arg string | string[]
----@return string[]
-local function to_array(arg)
-  if type(arg) == "string" then
-    return { arg }
-  else
-    return arg
-  end
-end
-
----@class PluginSpec
----@field [1] string
----@field branch string?
----@field tag string?
----@field event (string | string[])?
----@field cmd (string | string[])?
----@field ft (string | string[])?
----@field config function?
-
----@class Plugin
----@field name string
----@field url string
----@field path string
----@field config function?
----@field branch string?
----@field tag string?
----@field event string[]?
----@field cmd string[]?
----@field ft string[]?
-Plugin = {}
-
----@param spec string | PluginSpec
-function Plugin:new(spec)
-  spec = to_array(spec)
-  ---@cast spec PluginSpec
-  ---@type Plugin
-  local plugin = setmetatable({}, self)
-  self.__index = self
-
-  plugin.name = vim.split(spec[1], "/")[2]
-  plugin.url = string.format("https://github.com/%s.git", spec[1])
-  plugin.path = AlpacaPath .. ((spec.event or spec.cmd or spec.ft) and "/opt/" or "/start/") .. plugin.name
-  plugin.config = spec.config
-  plugin.event = spec.event and to_array(spec.event)
-  plugin.cmd = spec.cmd and to_array(spec.cmd)
-  plugin.ft = spec.ft and to_array(spec.ft)
-  plugin.branch = spec.branch
-  plugin.tag = spec.tag
-
-  return plugin
-end
-
----@return boolean
-function Plugin:needs_installed()
-  if uv.fs_stat(self.path) then
-    return false
-  else
-    return true
-  end
-end
-
----@param args string[]
----@param cwd string?
----@param callback fun(stderr: string?, stdout: string?): nil
-local function git(args, cwd, callback)
-  local stdout = uv.new_pipe(false)
-  local stderr = uv.new_pipe(false)
-  local handle, _ = uv.spawn("git", {
-    args = args, cwd = cwd, stdio = { nil, stdout, stderr }
-  }, function(code, signal)
-    local buffer = ""
-    if code == 0 then
-      stdout:read_start(function(err, data)
-        if err then
-          callback(err, nil)
-        elseif data then
-          buffer = buffer .. data
-        else
-          callback(nil, buffer)
-        end
-      end)
-    else
-      stderr:read_start(function(err, data)
-        if err then
-          callback(err, nil)
-        elseif data then
-          buffer = buffer .. data
-        else
-          callback(buffer, nil)
-        end
-      end)
-    end
-  end)
-  if not handle then
-    callback("Failed to start git", nil)
-  end
-end
-
----@return boolean
-function Plugin:needs_update()
-  git({ "fetch", "origin", "--tags" }, self.path, function(err, out)
-  end)
-  return true
-end
-
----@param callback fun(err: string?): nil
-function Plugin:install(callback)
-  git({ "clone", self.url, self.path, "--depth=1", "--recurse-submodules", "--shallow-submodules" }, nil,
-    function(err, out)
-      if err then callback(err) end
-      if out then print(out) end
-      git({ "fetch", "--tags", "origin" }, self.path, function(err, out)
-        if err then callback(err) end
-        if out then print(out) end
-        if self.branch then
-          git({ "checkout", self.branch }, self.path, function(err, out)
-            if err then callback(err) end
-            if out then print(out) end
-            callback(nil)
-          end)
-        elseif self.tag then
-          local newest = self:find_newest_tag()
-          git({ "checkout", newest }, self.path, function(err, out)
-            if err then callback(err) end
-            if out then print(out) end
-            callback(nil)
-          end)
-        end
-      end)
-    end)
-end
-
----@param callback fun(err: string?): nil
-function Plugin:update(callback)
-end
-
-local M = {}
-
----@param configs (string | PluginSpec)[]
-function M.setup(configs)
-  vim.api.nvim_create_augroup("AlpacaLazy", { clear = true })
-
-  vim.iter(configs):map(function(config)
-    return Plugin:new(config)
-  end):each(function(plugin)
-    if plugin:needs_installed() then
-      vim.print(plugin)
-      plugin:install(function(err)
-        if err then
-          vim.schedule_wrap(function()
-            vim.print(err)
-          end)
-        end
-      end)
-    end
-  end)
-
-  -- Plugins:setup(configs)
-  -- Installed:setup()
-end
-
-return M
