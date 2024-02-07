7b30a98ba19eaee4efdf8363fb7806c16dee7f78
ripjackie <theranslinkard@gmail.com>
2024-02-07 10:11:47 -0600

    working slowly and steadily

--- lua/alpaca.lua
+++ lua/alpaca.lua
@@ -2,6 +2,7 @@ local vim = vim
 local uv = vim.uv or vim.loop
 
 AlpacaPath = vim.fn.stdpath("data") .. "/site/pack/alpaca"
+AlpacaLog  = vim.fn.stdpath("cache") .. "/alpaca.log"
 
 local utils = {}
 
@@ -50,25 +51,34 @@ function utils:git_clone(plugin, callback)
 end
 
 function utils.log(level, message)
-  -- style is LEVEL | MESSAGE | TIMESTAMP
-  -- so ERROR | Failed to spawn Git Spawn with args { init, ... } | 2024-08-02 3:07:45PM
+  local file = assert(io.open(AlpacaLog, 'w'))
+  file:write(string.format("%s | %s | %s", string.upper(level), message, os.date('%c', os.time())))
+  file:close()
 end
-
 
 ---@class PluginSpec
 ---@field [1] string plugin location (this only allows for git.)
 ---@field as string? plugin alias
 ---@field config function? plugin config to run on load
 ---@field build (function | string)? object to run after install / update
----@field vers {branch: string?, tag: string?}? local repo versioning scheme ( tag allows semver using vim.version )
----@field opt {event: (string|string[])?, cmd: (string|string[])?, ft: (string|string[])?}? lazy loading & mechanisms
+---@field branch string? branch to follow
+---@field tag string? tag semver to follow
+---@field event (string|string[])? lazy load on event(s)
+---@field cmd (string|string[])? lazy load on user cmd(s)
+---@field ft (string|string[])? lazy load on filetype(s)
 
 
 ---@class Plugin
 ---@field name string name / alias for plugin internally
 ---@field path string path plugin is installed to
 ---@field installed boolean if plugin is currently installed or not
----@field spec PluginSpec spec associated with plugin
+---@field config function? function to run on plugin load
+---@field build (function | string)? command to run after install / update
+---@field branch string? branch to follow
+---@field tag string? tag semver to follow
+---@field event string[]? lazy load on events
+---@field cmd string[]? lazy load on user cmds
+---@field ft string[]? lazy load on filetypes
 local Plugin = {}
 
 ---@param spec PluginSpec
@@ -78,23 +88,106 @@ function Plugin:new(spec)
 
   plugin.name = spec.as or vim.split(spec[1], '/')[2]
   plugin.url = "https://github.com/" .. spec[1] .. ".git"
-  plugin.path = vim.fs.joinpath(AlpacaPath, (spec.opt and "opt" or "start"), plugin.name)
+  plugin.path = vim.fs.joinpath(AlpacaPath, ((spec.event or spec.cmd or spec.ft) and "opt" or "start"), plugin.name)
   plugin.installed = uv.fs_stat(plugin.path) and true or false
 
-  plugin.spec = spec
+  plugin.branch = spec.branch
+  plugin.tag = spec.tag
+
+  plugin.config = spec.config
+  plugin.build = spec.build
+
+  plugin.event = spec.event and type(spec.event) ~= "table" and {spec.event} or spec.event --[=[@as string[]?]=]
+  plugin.cmd = spec.cmd and type(spec.cmd) ~= "table" and {spec.cmd} or spec.cmd --[=[@as string[]?]=]
+  plugin.ft = spec.ft and type(spec.ft) ~= "table" and {spec.ft} or spec.ft --[=[@as string[]?]=]
 
   self.__index = self
   return setmetatable(plugin, self)
+end
 
+function Plugin:check_updates(callback)
+  if self.tag then
+    -- get current tag ( git describe --tags --exact-match )
+    -- get remote tags ( git ls-remote --refs --tags --sort=-v:refname --quiet origin )
+    -- if there is a newer remote tag that follows the plugin.tag semver, return true
+    print()
+  elseif self.branch then
+    -- get current head ( git rev-parse [HEAD?] )
+    -- get current symbolic-ref (may not be needed) ( git symbolic-ref HEAD )
+    -- get current remote head ( git ls-remote --refs --heads --quiet plugin.branch ( symbolic-ref? ) )
+    -- if remote head is different, return true
+    print()
+  else
+    -- get current head ( git rev-parse HEAD )
+    -- get current head ( git rev-parse [HEAD?] )
+    -- get current symbolic-ref (may not be needed) ( git symbolic-ref HEAD )
+    -- get current remote head ( git ls-remote --refs --heads --quiet plugin.branch ( symbolic-ref? ) )
+    -- if remote head is different, return true
+    print()
+  end
 end
 
 local M = {}
 
+M.tests = {
+  setup = function()
+    M.setup({
+      { "sainnhe/everforest" },
+      {
+        "catppuccin/nvim",
+        as = "catppuccin"
+      },
+      {
+        "lukas-reineke/indent-blankline.nvim",
+        event = "BufEnter",
+        tag = "v3.5.x",
+        config = function()
+          require("ibl").setup({})
+        end
+      },
+      {
+        "altermo/ultimate-autopair.nvim",
+        event = { "InsertEnter", "CmdlineEnter" },
+        branch = "v0.6",
+        config = function()
+          require("ultimate-autopair").setup({})
+        end
+      },
+      {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+          require("nvim-treesitter.configs").setup({
+            ensure_installed = { "lua", "vimdoc", "query", "markdown", "markdown_inline" },
+            highlight = { enable = true },
+            indent = { enable = true }
+          })
+        end
+      },
+      {
+        "zaldih/themery.nvim",
+        cmd = "Themery",
+        config = function()
+          require("themery").setup({
+            themes = require("themes"),
+            themeConfigFile = vim.fn.stdpath("config") .. "/lua/theme.lua",
+            livePreview = true
+          })
+        end
+      }
+    })
+  end
+}
+
+---@param specs (string | PluginSpec)[]
 function M.setup(specs)
   for _, spec in ipairs(specs) do
-    spec = type(spec) ~= "table" and {spec} or spec
+    spec = type(spec) ~= "table" and {spec} or spec --[[@as PluginSpec]]
     local plugin = Plugin:new(spec)
     vim.print(plugin)
+    if not plugin.installed then
+
+    end
   end
 end
 
