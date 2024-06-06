acbdd00ec333434d17957134cd3317ba43970407
ripjackie <theranslinkard@gmail.com>
2024-06-06 03:53:38 -0500

    workin on minimal version

--- lua/alpaca.lua
+++ lua/alpaca.lua
@@ -1,213 +1,129 @@
 local vim = vim
 local uv = vim.uv or vim.loop
-local git = require("git")
-local Plugin = require("plugin")
+local alpacapath = vim.fn.stdpath("data") .. "/site/pack/alpaca"
 
-local PluginPath = vim.fn.stdpath("data") .. "/site/pack/alpaca"
+
+local git = {}
+
+git.spawn = function (args, cwd, callback)
+  local stdout = uv.new_pipe(false)
+  local stderr = uv.new_pipe(false)
+  local buffers = { stdout = "", stderr = "" }
+
+  local on_pipe = function (buffer)
+    return function (err, data)
+      if err then
+        return callback(false, err)
+      elseif data then
+        buffers[buffer] = buffers[buffer] .. data
+      end
+    end
+  end
+  local on_exit = function (code)
+    local ok = code == 0
+    return callback(ok, ok and buffers.stdout or buffers.stderr)
+  end
+
+  local handle = uv.spawn("git", {
+    args = args, stdio = { nil, stdout, stderr }, cwd = cwd, verbatim = true
+  }, on_exit)
+
+  assert(handle, "Failed to spawn git with args: " .. vim.inspect(args))
+
+  stdout:read_start(on_pipe("stdout"))
+  stderr:read_start(on_pipe("stderr"))
+end
+
+git.cospawn = function (args, cwd)
+  local coro = coroutine.running()
+  assert(coro, "Failed to find Coroutine")
+  git.spawn(args, cwd, function (ok, out)
+    return coroutine.resume(coro, ok, out)
+  end)
+  return coroutine.yield()
+end
+
+git.rev_parse = function (path)
+  return git.cospawn({ "rev-parse", "HEAD" }, path)
+end
+
+git.get_remote_url = function (path)
+  return git.cospawn({ "ls-remote", "--get-url" }, path)
+end
+
+git.describe = function (path)
+  return git.cospawn({ "describe", "--all", "--exact-match" }, path)
+end
+
 
 local util = {}
-function util.to_table(value)
-  return type(value) ~= "table" and {value} or value
-end
 
-function util.log(msg, level)
-  vim.schedule(function()
-    vim.notify("[Alpaca.nvim] " .. msg, level)
-  end)
-end
-
-util.logger = {
-  init = function (self, method)
-    self.__index = self
-    return setmetatable({
-      total = 0,
-      index = 0,
-      errors = {},
-      method = method
-    }, self)
-  end,
-  track = function (self, name, on_success)
-    if self.total == 0 then
-      print(("[Alpaca] [%s] Begin"):format(self.method))
-    end
-    self.total = self.total + 1
-    return function (ok, out)
-      self.index = self.index + 1
-      if ok then
-        -- [Alpaca] [Install] [1/14] Success: sainnhe/everforest
-        -- [Alpaca] [Install] [8/14] Failure: sainnhe/sonokai
-        print(("[Alpaca] [%s] [%d/%d] %s %s"):format(self.method, self.index, self.total, "Success", name))
-        vim.schedule(on_success)
-      else
-        print(("[Alpaca] [%s] [%d/%d] %s %s"):format(self.method, self.index, self.total, "Failure", name))
-        table.insert(self.errors, { name = name, err = out })
-      end
-      if self.index == self.total then
-        print(("[Alpaca] [%s] Done%s"):format(self.method, #self.errors > 0 and " w/ Errors" or ""))
-        if #self.errors > 0 then
-          print(vim.inspect(self.errors))
-        end
-      end
-    end
-  end
-}
-
-
----@class Alpaca
----@field plugins Plugin[]
----@field opts table
-local Alpaca = {
-  plugins = {},
-  opts = {
-    install_on_start = true
-  }
-}
-
-function Alpaca:install()
-  local logger = util.logger:init("Install")
-  for _, plugin in pairs(self.plugins) do
-    if plugin.spec and not plugin.install then
-      plugin:do_install(logger:track(plugin.spec.name, function ()
-        vim.cmd("let &rtp = &rtp")
-        plugin:do_build()
-        plugin:do_load()
-      end))
-    end
-  end
-end
-
-function Alpaca:update() -- TODO
-  local total = 0
-  local index = 0
-  for _, plugin in pairs(self.plugins) do
-    if plugin.spec and plugin.install then
-      if total == 0 then
-        print("Updating Plugins")
-      end
-      total = total + 1
-      plugin:do_update(function (ok, out)
-        if ok then
-          print("Update Done " .. plugin.spec.name)
-          plugin:do_build()
-        else
-          print("Update Fail " .. out)
-        end
-        if index == total then
-          print("all Updates Complete")
-        end
-      end)
-    end
-  end
-end
-
-function Alpaca:clean() --TODO
-  local total = 0
-  local index = 0
-  for _, plugin in pairs(self.plugins) do
-    if not plugin.spec and plugin.install then
-      if total == 0 then
-        print("Cleaning Plugins")
-      end
-      total = total + 1
-      plugin:do_clean(function (ok, out)
-        if ok then
-          print("Clean Done " .. plugin.spec.name)
-        else
-          print("Clean Fail " .. out)
-        end
-        if index == total then
-          print("all Cleaning Complete")
-        end
-      end)
-    end
-  end
-end
-
-function Alpaca:setup(specs, opts)
-  -- Init Steps
-  vim.validate({
-    specs = { specs, "table" },
-      opts = { opts, { "table", "nil" } }
-  })
-
-  if opts then
-    vim.tbl_deep_extend("force", self.opts, opts)
-  end
-
-  -- Parse Specs
-  for _, spec in ipairs(specs) do
-    spec = util.to_table(spec)
-    local plugin = Plugin:from_spec(spec)
-    if plugin then
-      self.plugins[plugin.repo] = plugin
-    end
-  end
-
-  -- Parse Installs
-  for filename, filetype in vim.fs.dir(PluginPath, { depth = 2 }) do
-    if filetype == "directory" and filename:find("/") then
-      local plugin_path = ("%s/%s"):format(PluginPath, filename)
-      local _, remote = git.get_url(plugin_path)
-      local repo = remote:match("https://github.com/(%C+/%C+).git")
-      local _, commit = git.rev_parse(plugin_path)
-      local _, ref = git.describe(plugin_path)
-      local install = {
-        repo = repo,
-        opt = filename:match("opt/%C+") and true or false,
-        path = plugin_path,
-        commit = commit:match("(%w+)"),
-        branch = ref:match("heads/(%C+)"),
-        tag = ref:match("tags/(%C+)")
-      }
-      if self.plugins[repo] then
-        self.plugins[repo].install = install
-      else
-        self.plugins[repo] = Plugin:from_install(install)
-      end
-    end
-  end
-
-  if self.opts.install_on_start then
-    vim.schedule(function ()
-      Alpaca:install()
-    end)
+util.to_table = function (input)
+  if type(input) ~= "table" then
+    return {input}
   else
-    vim.schedule(function ()
-      vim.api.nvim_create_user_command("AlpacaInstall", function ()
-        vim.schedule(function ()
-          coroutine.wrap(Alpaca.install)(Alpaca)
-        end)
-      end, {})
-    end)
+    return input
   end
+end
 
-  vim.schedule(function ()
-    vim.api.nvim_create_user_command("AlpacaUpdate", function ()
-      vim.schedule(function ()
-        Alpaca:update()
-      end)
-    end, {})
-    vim.api.nvim_create_user_command("AlpacaClean", function ()
-      vim.schedule(function ()
-        Alpaca:clean()
-      end)
-    end, {})
-  end)
 
-  -- load all installed plugins
-  for _, plugin in pairs(self.plugins) do
-    if plugin.spec and plugin.install then
-      plugin:do_load()
+local plugins = {}
+
+plugins.find = function (self, remote)
+  for index, plugin in ipairs(self) do
+    if plugin.remote == remote then
+      return index
     end
   end
-
-  -- Setup Complete
 end
+
+plugins.insert = function (self, plugin)
+  local remote = plugin.spec and plugin.spec.url or plugin.install and plugin.install.remote
+  assert(remote, "[Alpaca] Failed to find remote in plugin " .. vim.inspect(plugin))
+  local index = self:find(remote)
+  if not index then
+    plugin.remote = remote
+    table.insert(self, plugin)
+  elseif plugin.install and not plugin.spec then
+    self[index].install = plugin.install
+  end
+end
+
 
 local M = {}
 
-function M.setup(specs, opts)
-  coroutine.wrap(Alpaca.setup)(Alpaca, specs, opts)
+M._setup = function (specs)
+
+  for _, spec in ipairs(specs) do
+    spec = util.to_table(spec)
+    spec.url = spec.url or spec[1] and ("https://github.com/%s.git"):format(spec[1]:match("(%C+/%C+)"))
+    assert(spec.url, "[Alpaca] Failed to find remote url in spec for " .. vim.inspect(spec))
+    spec.name = spec.as or spec.url:match("https://%C+/%C+/(%C+)"):gsub(".git", "")
+    spec.opt = (spec.event or spec.cmd or spec.ft) ~= nil
+    spec.path = alpacapath .. (spec.opt and "/opt/" or "/start/") .. spec.name
+    plugins:insert({ spec = spec })
+  end
+
+  for filename, filetype in vim.fs.dir(alpacapath, { depth = 2 }) do
+    if filetype == "directory" and filename:find("/") then
+      local fullpath = alpacapath .. "/" .. filename
+      local ok, commit = git.rev_parse(fullpath)
+      local ok, remote = git.get_remote_url(fullpath)
+      local ok, ref = git.describe(fullpath)
+      print(vim.inspect({
+        fullpath = fullpath,
+        commit = commit,
+        remote = remote,
+        ref = ref
+      }))
+    end
+  end
+
+  print(vim.inspect(plugins))
+end
+
+M.setup = function (specs)
+  coroutine.wrap(M._setup)(specs)
 end
 
 return M
---
+++ lua/alpaca_old.lua
@@ -0,0 +1,213 @@
+local vim = vim
+local uv = vim.uv or vim.loop
+local git = require("git")
+local Plugin = require("plugin")
+
+local PluginPath = vim.fn.stdpath("data") .. "/site/pack/alpaca"
+
+local util = {}
+function util.to_table(value)
+  return type(value) ~= "table" and {value} or value
+end
+
+function util.log(msg, level)
+  vim.schedule(function()
+    vim.notify("[Alpaca.nvim] " .. msg, level)
+  end)
+end
+
+util.logger = {
+  init = function (self, method)
+    self.__index = self
+    return setmetatable({
+      total = 0,
+      index = 0,
+      errors = {},
+      method = method
+    }, self)
+  end,
+  track = function (self, name, on_success)
+    if self.total == 0 then
+      print(("[Alpaca] [%s] Begin"):format(self.method))
+    end
+    self.total = self.total + 1
+    return function (ok, out)
+      self.index = self.index + 1
+      if ok then
+        -- [Alpaca] [Install] [1/14] Success: sainnhe/everforest
+        -- [Alpaca] [Install] [8/14] Failure: sainnhe/sonokai
+        print(("[Alpaca] [%s] [%d/%d] %s %s"):format(self.method, self.index, self.total, "Success", name))
+        vim.schedule(on_success)
+      else
+        print(("[Alpaca] [%s] [%d/%d] %s %s"):format(self.method, self.index, self.total, "Failure", name))
+        table.insert(self.errors, { name = name, err = out })
+      end
+      if self.index == self.total then
+        print(("[Alpaca] [%s] Done%s"):format(self.method, #self.errors > 0 and " w/ Errors" or ""))
+        if #self.errors > 0 then
+          print(vim.inspect(self.errors))
+        end
+      end
+    end
+  end
+}
+
+
+---@class Alpaca
+---@field plugins Plugin[]
+---@field opts table
+local Alpaca = {
+  plugins = {},
+  opts = {
+    install_on_start = true
+  }
+}
+
+function Alpaca:install()
+  local logger = util.logger:init("Install")
+  for _, plugin in pairs(self.plugins) do
+    if plugin.spec and not plugin.install then
+      plugin:do_install(logger:track(plugin.spec.name, function ()
+        vim.cmd("let &rtp = &rtp")
+        plugin:do_build()
+        plugin:do_load()
+      end))
+    end
+  end
+end
+
+function Alpaca:update() -- TODO
+  local total = 0
+  local index = 0
+  for _, plugin in pairs(self.plugins) do
+    if plugin.spec and plugin.install then
+      if total == 0 then
+        print("Updating Plugins")
+      end
+      total = total + 1
+      plugin:do_update(function (ok, out)
+        if ok then
+          print("Update Done " .. plugin.spec.name)
+          plugin:do_build()
+        else
+          print("Update Fail " .. out)
+        end
+        if index == total then
+          print("all Updates Complete")
+        end
+      end)
+    end
+  end
+end
+
+function Alpaca:clean() --TODO
+  local total = 0
+  local index = 0
+  for _, plugin in pairs(self.plugins) do
+    if not plugin.spec and plugin.install then
+      if total == 0 then
+        print("Cleaning Plugins")
+      end
+      total = total + 1
+      plugin:do_clean(function (ok, out)
+        if ok then
+          print("Clean Done " .. plugin.spec.name)
+        else
+          print("Clean Fail " .. out)
+        end
+        if index == total then
+          print("all Cleaning Complete")
+        end
+      end)
+    end
+  end
+end
+
+function Alpaca:setup(specs, opts)
+  -- Init Steps
+  vim.validate({
+    specs = { specs, "table" },
+      opts = { opts, { "table", "nil" } }
+  })
+
+  if opts then
+    vim.tbl_deep_extend("force", self.opts, opts)
+  end
+
+  -- Parse Specs
+  for _, spec in ipairs(specs) do
+    spec = util.to_table(spec)
+    local plugin = Plugin:from_spec(spec)
+    if plugin then
+      self.plugins[plugin.repo] = plugin
+    end
+  end
+
+  -- Parse Installs
+  for filename, filetype in vim.fs.dir(PluginPath, { depth = 2 }) do
+    if filetype == "directory" and filename:find("/") then
+      local plugin_path = ("%s/%s"):format(PluginPath, filename)
+      local _, remote = git.get_url(plugin_path)
+      local repo = remote:match("https://github.com/(%C+/%C+).git")
+      local _, commit = git.rev_parse(plugin_path)
+      local _, ref = git.describe(plugin_path)
+      local install = {
+        repo = repo,
+        opt = filename:match("opt/%C+") and true or false,
+        path = plugin_path,
+        commit = commit:match("(%w+)"),
+        branch = ref:match("heads/(%C+)"),
+        tag = ref:match("tags/(%C+)")
+      }
+      if self.plugins[repo] then
+        self.plugins[repo].install = install
+      else
+        self.plugins[repo] = Plugin:from_install(install)
+      end
+    end
+  end
+
+  if self.opts.install_on_start then
+    vim.schedule(function ()
+      coroutine.wrap(Alpaca.install)(Alpaca)
+    end)
+  else
+    vim.schedule(function ()
+      vim.api.nvim_create_user_command("AlpacaInstall", function ()
+        vim.schedule(function ()
+          coroutine.wrap(Alpaca.install)(Alpaca)
+        end)
+      end, {})
+    end)
+  end
+
+  vim.schedule(function ()
+    vim.api.nvim_create_user_command("AlpacaUpdate", function ()
+      vim.schedule(function ()
+        coroutine.wrap(Alpaca.update)(Alpaca)
+      end)
+    end, {})
+    vim.api.nvim_create_user_command("AlpacaClean", function ()
+      vim.schedule(function ()
+        coroutine.wrap(Alpaca.clean)(Alpaca)
+      end)
+    end, {})
+  end)
+
+  -- load all installed plugins
+  for _, plugin in pairs(self.plugins) do
+    if plugin.spec and plugin.install then
+      plugin:do_load()
+    end
+  end
+
+  -- Setup Complete
+end
+
+local M = {}
+
+function M.setup(specs, opts)
+  -- coroutine.wrap(Alpaca.setup)(Alpaca, specs, opts)
+end
+
+return M
